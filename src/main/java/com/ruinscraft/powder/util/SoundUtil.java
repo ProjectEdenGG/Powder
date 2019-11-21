979b1e7eb251119106caddf87c6e2a3ab35fee13
BuildTools <WakkaFlocka239@gmail.com>
2019-11-20 18:56:34 -0600

    Update to nbapi 1.4.4

--- src/main/java/com/ruinscraft/powder/util/SoundUtil.java
+++ src/main/java/com/ruinscraft/powder/util/SoundUtil.java
@@ -3,10 +3,10 @@ package com.ruinscraft.powder.util;
 import com.ruinscraft.powder.PowderPlugin;
 import com.ruinscraft.powder.model.Sound;
 import com.ruinscraft.powder.model.SoundEffect;
-import com.xxmicloxx.NoteBlockAPI.Layer;
-import com.xxmicloxx.NoteBlockAPI.NBSDecoder;
-import com.xxmicloxx.NoteBlockAPI.Note;
-import com.xxmicloxx.NoteBlockAPI.Song;
+import com.xxmicloxx.NoteBlockAPI.model.Layer;
+import com.xxmicloxx.NoteBlockAPI.utils.NBSDecoder;
+import com.xxmicloxx.NoteBlockAPI.model.Note;
+import com.xxmicloxx.NoteBlockAPI.model.Song;
 import com.xxmicloxx.NoteBlockAPI.utils.InstrumentUtils;
 
 import java.io.File;
@@ -52,8 +52,8 @@ public class SoundUtil {
         for (Integer integer : song.getLayerHashMap().keySet()) {
             Layer layer = song.getLayerHashMap().get(integer);
             for (int i = 0; i < volumeMultiplier; i++) {
-                for (Integer tick : layer.getHashMap().keySet()) {
-                    Note note = layer.getHashMap().get(tick);
+                for (Integer tick : layer.getNotesAtTicks().keySet()) {
+                    Note note = layer.getNotesAtTicks().get(tick);
                     String soundName = InstrumentUtils.getInstrumentName(note.getInstrument());
                     org.bukkit.Sound sound = Sound.getFromBukkitName(soundName);
                     float newVolume = (layer.getVolume() / 100F) * ((float) volume);
